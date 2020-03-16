a1f7ea879516b086521e1439e43246d5ecf6b549
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-03-16 19:31:23 +0330

    add a hello world!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("hello world!");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println(num1 + num2);
     }
 }
